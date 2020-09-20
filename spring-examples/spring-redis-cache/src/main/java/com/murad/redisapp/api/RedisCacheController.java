569a9208cdebc0a4a027dd0cdfbae3ac126c10d2
muradisgandar <isgandarli_murad@mail.ru>
2020-09-20 23:58:04 +0400

    spring redis cache example

---
+++ spring-examples/spring-redis-cache/src/main/java/com/murad/redisapp/api/RedisCacheController.java
@@ -0,0 +1,27 @@
+package com.murad.redisapp.api;
+
+import com.murad.redisapp.service.RedisCacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class RedisCacheController {
+
+    private int counter = 0;
+
+    @Autowired
+    private RedisCacheService redisCacheService;
+
+    @GetMapping
+    public String cacheControl() throws InterruptedException {
+        if(counter==5){
+            redisCacheService.clearCache();
+            counter = 0;
+        }
+        counter++;
+        return redisCacheService.longRunningMethod();
+    }
+}
